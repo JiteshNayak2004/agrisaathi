3194fff6cae70b8660d88381fc34db84f3ea7456
jitesh <jiteshnayak2004@gmail.com>
2023-03-02 21:34:24 +0530

    django

---
+++ venv/project3.py
@@ -0,0 +1,31 @@
+from flask import Flask, request,render_template,jsonify
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+# from PIL import Image
+# from numpy import asarray
+
+app = Flask(__name__)
+model=pickle.load(open('crop_recommender.pkl','rb'))
+# model1=pickle.load(open('Apple.pkl','rb'))
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/data',methods=['POST'])
+def predict():
+    int_features=[float(x) for x in request.form.values()]
+    # print ("printing"+request.values.get("N"))
+    final_features=[np.array(int_features)]
+    prediction=model.predict(final_features)
+    # print(prediction)
+    output=prediction[0]
+    print("Crop we recommend you is "+output)
+    # return render_template("index.html',prediction_text='Crop we recommend you to grow {}".format(output))
+    # print("Crop we recommend you is "+output)
+    return render_template('index.html', prediction_text='Crop we recommend is {}'.format(output))
+
+if __name__ == '__main__':
+    app.run(debug=True)
